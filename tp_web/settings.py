b6539bbb552b9d33de2044c87da60312be8b3edf
Andrea-Sanchez-Liporace <sanchezliporace.a@gmail.com>
2026-06-19 20:53:39 -0300

    Configuracion para produccion: Procfile, gunicorn, settings

--- tp_web/settings.py
+++ tp_web/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure--y_5u+4pchj^(47lvhfgcla_unlfu03*%6dw6z#c0f6#xcvvdn'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '.onrender.com']
 
 
 # Application definition
@@ -117,6 +117,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
